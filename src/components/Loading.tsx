e1b0794a477a4811a04308eb12a9809d46647362
Youngeun Jun <121297085+Junyewdd@users.noreply.github.com>
2024-04-03 00:00:35 +0900

    refactor: 초기 설정 다시 시도

--- src/components/Loading.tsx
+++
@@ -1,2 +0,0 @@
-// import {CircularProgress, Dialog}
-
---
+++ src/components/layout/AppLayout.tsx
@@ -0,0 +1,21 @@
+import { Box } from "@mui/material";
+
+const AppLayout = (props: { children: React.ReactNode }) => {
+  return (
+    <Box sx={{display: 'flex', justifyContent: 'center'}}>
+      <Box sx={{
+        width:{
+          xs: '100%',
+          md: '500px',
+        },
+        bgcolor: 'white',
+        }}>
+        <Box sx={{width: '100%', height: '100vh'}}>
+          {props.children}
+        </Box>
+      </Box>
+    </Box>
+  );
+};
+
+export default AppLayout;
