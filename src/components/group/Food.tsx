ec28ee5af87c708a22db87eb2b809f87f17d63fb
Youngeun Jun <121297085+Junyewdd@users.noreply.github.com>
2024-04-29 21:18:21 +0900

    feat: 재료 카드 구현 완료 (#13)

--- src/components/group/Food.tsx
+++ src/components/group/Food.tsx
@@ -1,9 +1,34 @@
+import { Box } from '@mui/material'
 import React from 'react'
+import EatTag from '../common/ingredients/EatTag';
+import IngredientCard from '../common/ingredients/IngredientCard';
 
 const Food = () => {
   return (
-    <div>Food</div>
+    <Box sx={containerStyle}>
+      <EatTag tag={'cannot eat'}/>
+      <Box sx={ingredientStyle}>
+        <IngredientCard name={'shrimp'} type={'seafood'}/>
+        <IngredientCard name={'shrimp'} type={'seafood'}/>
+        <IngredientCard name={'shrimp'} type={'seafood'}/>
+        <IngredientCard name={'shrimp'} type={'seafood'}/>
+      </Box>
+    </Box>
   )
 }
 
-export default Food
+export default Food;
+
+const containerStyle = {
+  width: '100%',
+  bgcolor: '#FFFFFF',
+  padding: '10px',
+
+};
+
+const ingredientStyle = {
+  display: 'flex',
+  flexDirection: 'row',
+  flexWrap: 'wrap',
+  justifyContent: 'flex-start',
+}
