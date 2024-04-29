ec7e58159d1c865583e7743cad15a5e0af502d3d
Youngeun Jun <121297085+Junyewdd@users.noreply.github.com>
2024-04-29 19:34:06 +0900

    feat: 멤버 카드 구현 (#13)

--- src/components/group/Group.tsx
+++ src/components/group/Group.tsx
@@ -3,10 +3,16 @@ import { Box, CardMedia } from '@mui/material';
 import { useSearchParams } from 'next/navigation';
 import React from 'react'
 import Member from './Member';
+import { useRouter } from 'next/router';
 
 const Group = () => {
+  const router = useRouter();
   const params = useSearchParams();
   const groupName = params.get('name');
+
+  const handleBack = () => {
+    router.push('/home');
+  }
   return (
     <Box sx={containerStyle}>
       <Box sx={backBoxStyle}>
@@ -15,16 +21,21 @@ const Group = () => {
           image={'/images/arrow-left.png'}
           title="profile"
           sx={backStyle}
+          onClick={handleBack}
           />
       </Box>
       <Box sx={groupNameStyle}>
         {groupName}
       </Box>
 
-      <Box sx={memberBoxStyle}>
-        <Member profile={'/images/myBlack.png'} name={'전영은'}/>
-        <Member profile={'/images/myBlack.png'} name={'전영은'}/>
-        <Member profile={'/images/myBlack.png'} name={'전영은'}/>
+      <Box sx={centerAlignBoxStyle}>
+        <Box sx={memberBoxStyle}>
+          <Member profile={'/images/myBlack.png'} name={'전영은'}/>
+          <Member profile={'/images/myBlack.png'} name={'전영은'}/>
+          <Member profile={'/images/myBlack.png'} name={'전영은'}/>
+          <Member profile={'/images/myBlack.png'} name={'전영은'}/>
+          <Member profile={'/images/myBlack.png'} name={'전영은'}/>
+        </Box>
       </Box>
     </Box>
   )
@@ -57,8 +68,19 @@ const groupNameStyle = {
   fontSize: '1.875rem',
   fontWeight: 'bold',
   paddingLeft: '1rem',
+  marginBottom: '2rem',
 };
 
 const memberBoxStyle = {
+  display: 'flex',
+  flexDirection: 'row',
+  flexWrap: 'wrap',
+  justifyContent: 'flex-start',
+  width: '85%',
+};
 
+const centerAlignBoxStyle = {
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
 }
--- src/components/group/Member.tsx
+++ src/components/group/Member.tsx
@@ -1,9 +1,44 @@
+import { Box, CardMedia } from '@mui/material'
 import React from 'react'
 
 const Member = ({profile, name}: {profile:string, name: string}) => {
   return (
-    <div>{name}</div>
+    <Box sx={memberContainer}>
+      <CardMedia
+          component="img"
+          image={profile}
+          title="profile"
+          sx={profileStyle}
+          />
+      {name}
+    </Box>
   )
 }
 
-export default Member
+export default Member;
+
+const memberContainer = {
+  width: '40%',
+  height: '120px',
+  bgcolor: 'white',
+  margin: '3%',
+
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'center',
+  alignItems: 'center',
+  boxShadow: '0px 4px 4px 0px rgba(0, 0, 0, 0.25)',
+  borderRadius: '0.9375rem',
+
+  fontFamily: 'Inter',
+  fontSize: '1.2rem',
+  fontStyle: 'normal',
+  fontWeight: '400',
+  lineHeight: 'normal',
+}
+
+const profileStyle = {
+width: '60px',
+height: '60px',
+margin: '10px',
+}
