372f698989b49bc99e19a2f7599cdd9a9f265ac4
Youngeun Jun <121297085+Junyewdd@users.noreply.github.com>
2024-05-02 19:15:24 +0900

    feat: go to order list 버튼 클릭 시 주문서로 이동 (#19)

--- src/components/menu/Foods.tsx
+++ src/components/menu/Foods.tsx
@@ -1,11 +1,19 @@
 import { Box } from '@mui/material'
 import React from 'react'
 import Food from './Food'
+import { setColor } from '@/utils/setColor';
+import { useRouter } from 'next/router';
 
 const Foods = ({sort}: {sort: string}) => {
+  const router = useRouter();
   const handleContents = () => {
 
-  }
+  };
+
+  const handleClick = () => {
+    router.push('/order');
+  };
+  
   return (
     <Box>
       {sort==='can eat' &&
@@ -34,8 +42,34 @@ const Foods = ({sort}: {sort: string}) => {
       <Food name = {'Altang (알탕)'}/>
       </>
       }
+      <Box sx={{...boxStyle, right: '15%','@media (min-width: 560px)': {
+          right: 'calc(50% - 200px)',
+        },}} onClick={handleClick}>
+        Go To Order List
+        </Box>
     </Box>
   )
 }
 
-export default Foods
+export default Foods;
+
+const boxStyle = {
+  width: '70%',
+  maxWidth: '400px',
+  height: '3.25rem',
+  position: 'fixed',
+  '@media (max-width: 560px)': {
+    left: '15%',
+  },
+  bottom: '40px',
+  bgcolor: setColor('main'),
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  color: 'white',
+  fontWeight: 'bold',
+  fontSize: '1.6rem',
+  borderRadius: '0.625rem',
+  cursor: 'pointer',
+  zIndex: 3,
+};
