e4d44896c4fdac16712de7bb6f0fd5d6a0f0920d
Youngeun Jun <121297085+Junyewdd@users.noreply.github.com>
2024-05-03 02:27:16 +0900

    feat: 그룹 멤버 조회 api 추가 (#14)

--- src/apis/group.ts
+++ src/apis/group.ts
@@ -1,14 +1,32 @@
 import axios from "axios";
 
 const baseURL = process.env.NEXT_PUBLIC_SERVER_URL;
+const token = process.env.NEXT_PUBLIC_TOKEN;
 
 export const getGroupList = async () => {
   try{
-    const response = await axios.get(`${baseURL}/groups`);
-    console.log(response);
+    const response = await axios.get(`${baseURL}/groups`, {
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    });
     return response.data;
     
   }catch(error){
     console.error(error);
   }
-}
+};
+
+export const getGroupMembers = async (groupName:string) => {
+  try{
+    const response = await axios.get(`${baseURL}/groups/${groupName}/members`, {
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    });
+    return response.data;
+    
+  }catch(error){
+    console.error(error);
+  }
+};
--- src/components/group/Home.tsx
+++ src/components/group/Home.tsx
@@ -1,15 +1,36 @@
 import { Box, CardMedia } from '@mui/material'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Member from './Member'
 import { useRouter } from 'next/router';
-
+import { getGroupMembers } from '@/apis/group';
+interface Member {
+  id: number;
+  name: string;
+}
 const Home = () => {
   const router = useRouter();
+  const [members, setMembers] = useState<Member[]>([]);
+  const groupName = Array.isArray(router.query.name) ? router.query.name[0] : router.query.name;
+  // useEffect(()=>{
+  //   const fetchData = async () => {
+  //     const response = await getGroupMembers(groupName ? groupName : '대박');
+  //     setMembers(response.data);
+  //   };
+  //   fetchData();
+  // }, []);
 
   return (
     <>
     <Box sx={centerAlignBoxStyle}>
       <Box sx={memberBoxStyle}>
+        {/* {members.map(member => (
+          <Member
+            key={member.id}
+            profile={`/images/groupGrey.png`}
+            name={member.name}
+            />
+        ))
+        } */}
         <Member profile={'/images/myBlack.png'} name={'전영은'}/>
         <Member profile={'/images/myBlack.png'} name={'전영은'}/>
         <Member profile={'/images/myBlack.png'} name={'전영은'}/>
