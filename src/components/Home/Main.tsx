4fddad2721d8c6853ea635c912b6dc90b022ff13
Youngeun Jun <121297085+Junyewdd@users.noreply.github.com>
2024-05-17 17:51:53 +0900

    feat: 한 메뉴 검색 기능 구현 (#31)

--- src/components/Home/Main.tsx
+++ src/components/Home/Main.tsx
@@ -1,14 +1,33 @@
-import { Box, CardMedia } from '@mui/material'
+import { Box, CardMedia, TextField } from '@mui/material'
 import { useRouter } from 'next/router'
-import React from 'react'
+import React, { useState } from 'react'
 
 const Main = () => {
   const router = useRouter();
+  const [menu, setMenu] = useState<string>('');
   const handleButton = () => {
     router.push('/menu');
+  };
+  const handleNicknameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setMenu(event.target.value);
+  };
+
+  const handleMenu = () => {
+    router.push(`http://localhost:3000/menu/detail?name=${menu}`);
   }
   return (
     <Box sx={boxStyle}>
+      <Box sx={searchStyle}>
+
+      <TextField sx={textFieldStyle} id='outlined-basic' label='food name' variant='outlined' onChange={handleNicknameChange}/>
+      <CardMedia
+        component="img"
+        image={'/images/search.png'}
+        title="profile"
+        sx={searchButtonStyle}
+        onClick={handleMenu}
+      />
+      </Box>
       <CardMedia
         component="img"
         image={'/images/cameraButton.png'}
@@ -28,9 +47,49 @@ const boxStyle = {
   display: 'flex',
   alignItems: 'center',
   justifyContent: 'center',
+  flexDirection: 'column',
 }
 
 const cameraStyle = {
   width: '200px',
   height: '200px',
+};
+
+const searchStyle = {
+  width: '60%',
+  height: '40px',
+  display: 'flex',
+  marginBottom: '50px',
+  alignItems: 'center',
+};
+
+const searchButtonStyle = {
+  width: '30px',
+  height: '30px',
+  marginLeft: '10px',
 }
+
+const textFieldStyle = {
+  width: '90%',
+  '& .MuiOutlinedInput-root': {
+    '& fieldset': {
+      borderColor: 'black',
+    },
+    '&:hover fieldset': {
+      borderColor: 'red',
+    },
+    '&.Mui-focused fieldset': {
+      borderColor: 'green',
+    },
+  },
+  '& .MuiInputBase-input': {
+    padding: '10px',
+
+  },
+  '& .MuiInputLabel-root': {
+    transform: 'translate(14px, 14px) scale(1)',
+    '&.MuiInputLabel-shrink': {
+      transform: 'translate(14px, -15px) scale(0.75)',
+    },
+  },
+};
