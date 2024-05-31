491b808a7f200484c5bccf51a5841e54474844d5
Youngeun Jun <121297085+Junyewdd@users.noreply.github.com>
2024-05-31 14:19:51 +0900

    refactor: 화질 다시 이전으로 수정

--- src/utils/camera.ts
+++ src/utils/camera.ts
@@ -1,11 +1,32 @@
+// export const getWebcam = (callback: (stream: MediaStream) => void): void => {
+//   const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+//   const constraints = {
+//     video: {
+//       facingMode: isMobile ? { exact: "environment" } : "user",
+//       width: { ideal: 1280 },
+//       height: { ideal: 720 },
+//     },
+//     audio: false
+//   };
+
+//   navigator.mediaDevices.getUserMedia(constraints)
+//     .then(callback)
+//     .catch((err) => {
+//       console.log(err);
+//     });
+// }
+
+// const Styles = {
+//   Video: { width: "100%", height: "100%", background: 'rgba(245, 240, 215, 0.5)' },
+//   None: { display: 'none' },
+// };
+
+// export default getWebcam;
+
 export const getWebcam = (callback: (stream: MediaStream) => void): void => {
   const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
   const constraints = {
-    video: {
-      facingMode: isMobile ? { exact: "environment" } : "user",
-      width: { ideal: 1280 },
-      height: { ideal: 720 },
-    },
+    video: { facingMode: isMobile ? { exact: "environment" } : "user" },
     audio: false
   };
 
